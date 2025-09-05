b97a465ffebfa466aabed2f20138e8e52c7ba8d1
Jaya Lakshmi Tangirala <jt3951@hallam.shu.ac.uk>
2025-09-05 13:07:19 +0100

    Add project files.

---
+++ IntegerStack.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackADT
+{
+    internal class IntegerStack
+    {
+        private int[] stack; //Array of elements
+        private int size;    //maximum number of elements in stack
+        private int top;     //index of top element in the stack.
+
+        public IntegerStack(int n)  //n is the maximum size of stack.
+        {
+            size = n;
+            stack = new int[size]; //allocate space to array of size n
+            top = -1;              // No elements in the stack initially.
+        }
+       public bool isEmpty()
+        {
+            if (top == -1)
+                return true;
+            else
+                return false;
+        }
+        public bool isFull()
+        {
+            if (top==size-1)
+                return true;
+            else 
+                return false;
+        }
+        public void push(int element)
+        {
+            if (isFull())
+                Console.WriteLine("Stack OverFlow!");
+            else
+            {
+                top++;
+                stack[top] = element;
+            }
+        }
+        public int pop() 
+        { 
+            if(isEmpty())
+            {
+                Console.WriteLine("Stack Underflow!!");
+                return -1;
+            }
+            else
+            {
+                int element = stack[top];
+                top--;
+                return element;
+            }   
+        }
+        public int peek()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack Empty");
+                return -1;
+            }                
+            else
+                return stack[top];
+        }
+        public void print()
+        {
+            if (isEmpty())
+                Console.WriteLine("Stack is empty");
+            else
+            {
+                Console.WriteLine("The elements of Stack are: ");
+                for (int i = top; i >= 0; i--)                
+                    Console.WriteLine(stack[i]);                
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿namespace StackADT
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Come let us test stack!");
+            Console.Write("Enter size of stack: ");
+            int size = int.Parse(Console.ReadLine());
+            IntegerStack s = new IntegerStack(size);
+
+            while (true)
+            {
+                Console.WriteLine("\n--- Menu ---");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Peek");
+                Console.WriteLine("4. Print Stack");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());                
+                int element;
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter value to push: ");
+                        element = int.Parse(Console.ReadLine());
+                        s.push(element);
+                        break;
+
+                    case 2:
+                        element = s.pop();
+                        if (element !=-1)
+                            Console.WriteLine("Popped: " + element);                        
+                        break;
+
+                    case 3:
+                        element = s.peek();
+                        if (element!=-1)
+                            Console.WriteLine("Top element: " + element);                        
+                        break;
+
+                    case 4:
+                        s.print();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Exiting program...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice! Please try again.");
+                        break;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
